f7e98b7dfd4c829325bfb16a0087ded25b14f2c3
kyoulee <kyoulee@student.42seoul.kr>
2025-10-04 07:09:50 +0900

    github favicon setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,26 @@ import type { Metadata } from "next";
 import { } from "next/font/google";
 import "./globals.css";
 
+import faviconLight from "@/app/favicon/favicon-light.svg"
+import faviconDark from "@/app/favicon/favicon-dark.svg"
+
 export const metadata: Metadata = {
   title: "Github Blog With Obsidian",
   description: "Develop by Kyoulee",
+  icons: {
+    icon: [
+      {
+        rel: 'icon',
+        url: faviconLight,
+        media: '(prefers-color-scheme: light)',
+      },
+      {
+        rel: 'icon',
+        url: faviconLight,
+        media: '(prefers-color-scheme: dark)',
+      }
+    ]
+  }
 };
 
 export default function RootLayout({
@@ -14,6 +31,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <link rel="icon" href="./favicon/favicon-dark.svg" type="image/svg" sizes="20" />
+      </head>
       <body
         className={`antialiased`}
       >
