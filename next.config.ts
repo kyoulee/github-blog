eee0fdd1a40675c2d9414e6d75b23597e65ef83d
kyoulee <kyoulee@student.42seoul.kr>
2025-11-09 17:24:26 +0900

    plugin remark-github , github-markdown-css, rehype-github-alert, rehype-pretty-code, shiki

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,19 @@
 import type { NextConfig } from "next";
 
 import createMDX from "@next/mdx";
+
 import remarkGfm from "remark-gfm";
-import rehypeKatex from "rehype-katex";
+import remarkGithub from "remark-github";
 import remarkMath from "remark-math";
+
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+
+import rehypeKatex from "rehype-katex";
+import { rehypeGithubAlerts } from "rehype-github-alerts";
+import rehypePrettyCode from "rehype-pretty-code";
+
+import config from "@/../config.json"
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -16,8 +24,18 @@ const nextConfig: NextConfig = {
 const withMDX = createMDX({
   extension: /\.(md|mdx)$/,
   options: {
-    remarkPlugins: [remarkFrontmatter, [remarkMdxFrontmatter, { name: "frontmatter" }], remarkGfm, remarkMath],
-    rehypePlugins: [rehypeKatex],
+    remarkPlugins: [
+      remarkFrontmatter,
+      [remarkMdxFrontmatter, { name: "frontmatter" }],
+      remarkGfm,
+      [remarkGithub, { repository: config.githubId + '/' + config.githubRepo }],
+      remarkMath
+    ],
+    rehypePlugins: [
+      rehypeKatex,
+      rehypeGithubAlerts,
+      [rehypePrettyCode, { theme: 'github-dark', },]
+    ],
   },
 });
 
